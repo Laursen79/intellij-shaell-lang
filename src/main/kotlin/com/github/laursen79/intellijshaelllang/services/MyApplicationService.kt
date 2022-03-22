92107f999062dc185e58d804b7bef85c36fd1870
GitHub Action <action@github.com>
2022-03-22 12:45:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/laursen79/intellijshaelllang/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.laursen79.intellijshaelllang.services
+
+import com.github.laursen79.intellijshaelllang.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
